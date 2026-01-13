954a538ae9bd1504cb41dc99b5d8698076b8c90b
sakthi904761 <234429481+sakthi904761@users.noreply.github.com>
2026-01-13 12:18:55 +0530

    Update classRouter.js

--- backend/router/classRouter.js
+++ backend/router/classRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getAllClasses, createClass } from "../controllers/classConroller.js";
+import { getAllClasses, createClass, updateClass, deleteClass } from "../controllers/classConroller.js";
 
 const router = express.Router();
 
 router.get('/getall', getAllClasses);
 router.post('/', createClass);
-
+router.put('/:id', updateClass);
+router.delete('/:id', deleteClass);
 
 export default router;
 
