4697346b4392f028bed6f30d903bd1a2856c1959
sakthi904761 <234429481+sakthi904761@users.noreply.github.com>
2026-01-20 09:20:38 -0800

    Add deleteEvents route to eventsRouter

--- backend/router/eventsRouter.js
+++ backend/router/eventsRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllEvents, createEvents } from "../controllers/eventsController.js";
+import { getAllEvents, createEvents, deleteEvents } from "../controllers/eventsController.js";
 
 const router = express.Router();
 
 router.get('/getall', getAllEvents);
 router.post('/', createEvents);
+router.delete('/:id', deleteEvents);
 
 
 export default router;
